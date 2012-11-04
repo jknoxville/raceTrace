d18b4c78397d766f06bb4533a26b795c7a8103ab
John Knox <jk510@cam.ac.uk>
2012-11-04 21:47:30 +0000

    Further additions to UML diagram

---
+++ project/src/uk/ac/cam/jk510/part2project/map/InvalidDataPointException.java
@@ -0,0 +1,5 @@
+package uk.ac.cam.jk510.part2project.map;
+
+public class InvalidDataPointException extends Exception {
+
+}
--- project/src/uk/ac/cam/jk510/part2project/settings/Config.java
+++ project/src/uk/ac/cam/jk510/part2project/settings/Config.java
@@ -3,7 +3,7 @@ package uk.ac.cam.jk510.part2project.settings;
 public class Config {
 	
 	static public int getMapLineThickness() {
-		return 30;
+		return 10;
 	}
 
 }
